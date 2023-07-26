45bb105295e42db2c6bc0a89ea0be1e8f105a4d7
geearen <allengonzales018@gmail.com>
2023-07-26 14:03:40 -0700

    error on vercel deployment - unable to find module ./store

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import store from "./redux/store";
+import store from "./redux/store.js";
 
 import "./index.css";
 
