031526370fe8670384fca82f12c09ad2f7afeb85
geearen <allengonzales018@gmail.com>
2023-07-25 23:15:35 -0700

    add action types and replace createStore (deprecated) with configureStore

---
+++ src/redux/actionTypes/actionTypes.ts
@@ -0,0 +1,3 @@
+export const NUM_WINS = "NUM_WINS";
+export const WIN_STATUS = "WIN_STATUS";
+export const GUESS_EXCEEDED = "GUESS_EXCEEDED";
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
-import { createStore } from "redux";
+import { configureStore } from "redux";
 import reducer from "./reducers";
 
-const store = createStore(
+const store = configureStore(
   reducer,
   typeof window === "object" &&
     typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
